d9ad4ad3cae459bb83cc6b45ed1989e744ccb831
Craig Spargo <cspargo@gmail.com>
2018-04-06 13:04:33 -0700

    Merged files into one repo.

---
+++ strcmp.py
@@ -0,0 +1,9 @@
+oldVersion = "11.2.2"
+newVersion = "11.3"
+
+if(newVersion == oldVersion):
+    print(newVersion + "is equal to " + oldVersion)
+if (newVersion > oldVersion):
+    print(newVersion + " is greater than " + oldVersion)
+if (newVersion < oldVersion):
+    print(newVersion + " is less than " + oldVersion)
