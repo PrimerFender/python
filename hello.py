b84260c500ae09e5bfd42b925d7005c6a1d8fd82
Craig Spargo <craig.spargo@apria.com>
2018-08-03 09:23:06 -0700

    Updated to be more generic.

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
-you = "Shannon"
+my_var = "Eat pie!"
+the_world_is_flat = True
 
-print("Who do you love? " + you)
+print("What did you say? " + my_var)
+
+if the_world_is_flat:
+    print("Be careful to not fall off!")
